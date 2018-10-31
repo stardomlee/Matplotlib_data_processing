2b139cbc3eedfaad89e122ecf66ca7ba12f88082
Joeylee1990s <lihuan12593@gmail.com>
2018-10-31 19:12:52 +0900

    savefigure.py
    This script can be inserted to your python code. It can save your figure which can be placed the position you want.

---
+++ savefigure.py
@@ -0,0 +1,13 @@
+import os
+import matplotlib.pyplot as plt
+
+script_dir = os.path.dirname(__file__)
+results_dir = os.path.join(script_dir, 'Results/')
+#plot_name = "sample"
+
+if not os.path.isdir(results_dir):
+    os.makedirs(results_dir)
+
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+plt.savefig(results_dir + 'plot_name.eps', dpi=300)
