b95bce2458e8f05b42f9a804403315a7d33a4010
Joeylee1990s <lihuan12593@gmail.com>
2018-11-07 11:22:03 +0900

    Update save_variables.py

--- save_variables.py
+++ save_variables.py
@@ -3,7 +3,8 @@ import numpy as np
 with open('numbers.txt', 'w') as handle:
     for n in np.arange(1, 5, 0.1):
         handle.write('{}\n'.format(n))
------------------------------------------------------
+        
+#################################################
 
 f = open("test.txt","wb")                                                                                                                                                                           
 for i in range(1,50,5):                                                                                                                                                                             
