1c1bc39775df4854fa446ceb3cdded069c1c7745
Joeylee1990s <lihuan12593@gmail.com>
2018-11-07 11:24:01 +0900

    Update save_variables.py

--- save_variables.py
+++ save_variables.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+###############################################
+#In Python, range(start, stop + 1, step) can be used like Matlab's start:step:stop command. 
+#Unlike Matlab's functionality, however, range only works when start, step, and stop are 
+#all integers. If you want a parallel function that works with floating-point values, try 
+#the arange command from numpy:
+################################################
+
 with open('numbers.txt', 'w') as handle:
     for n in np.arange(1, 5, 0.1):
         handle.write('{}\n'.format(n))
